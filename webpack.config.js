cd5c9e312af797e0711831c9db19f96a898983f9
Sunit Mody <sunitmody@gmail.com>
2020-11-12 13:31:31 -0800

    Set up server

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: __dirname + '/client/src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/client/dist'
+***REMOVED***
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+        ***REMOVED***
+      ***REMOVED***
+    ***REMOVED***
+***REMOVED***
+***REMOVED***
+  devtool: 'source-map'
+***REMOVED***;
+
+
+
+
+
