ed551b67bca16374d162d033513e2e645d3ab97e
Sunit Mody <sunitmody@gmail.com>
2020-11-14 14:59:46 -0800

    Create schema.sql seed file

--- database/index.js
+++ database/index.js
@@ -1,2 +1,19 @@
 // brew services start postgresql
 // psql postgres
+// CREATE ROLE root;
+// ALTER ROLE root CREATEDB;
+// psql -d postgres -U root (connect to postgres as user root)
+// psql postgres < database/schema.sql (run schema as superuser)
+// \l - list database
+// \c - connect to database
+// SELECT***REMOVED*** FROM items
+
+const { Pool ***REMOVED*** = require('pg');
+
+const pool = new Pool({
+  // user: 'root',
+  host: 'localhost',
+  database: 'fetsy',
+***REMOVED***);
+
+module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
+/* eslint-disable no-unused-vars***REMOVED***/
 /* eslint-disable no-console***REMOVED***/
 const express = require('express');
+const db = require('../database/index.js');
 
 const app = express();
 const port = 3004;
 
 app.use(express.static('client/dist'));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('GET request received at endpoint "/"');
