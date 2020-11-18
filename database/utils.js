8ece0f3327708a40859c9f5cce00f2eb982617e5
Sunit Mody <sunitmody@gmail.com>
2020-11-17 22:28:18 -0800

    Create random data utils, seed and update schema

---
+++ database/utils.js
@@ -0,0 +1,30 @@
+/* eslint-disable max-len***REMOVED***/
+/* eslint-disable no-plusplus***REMOVED***/
+// This file contain functions that create sample data
+// There should be a function that creates a row of sample data
+// A query should be run that adds the sample data to the database once it is created
+
+const wordBank = 'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor icididunt ut labore et dolore magna aliqua ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip exea commodo consequat duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur';
+
+const randomInt = (min, max) => Math.floor(Math.random()***REMOVED*** (max - min + 1) + min);
+
+const randomFloat = (min, max, decimalPlaces) => (Math.random()***REMOVED*** (max - min) + min).toFixed(decimalPlaces);
+
+const itemName = () => {
+  const words = wordBank.split(' ');
+  let itemNameLength = randomInt(3, 10);
+  let name = '';
+  while (itemNameLength > 0) {
+    name += `${words[randomInt(0, words.length - 1)]***REMOVED*** `;
+    itemNameLength--;
+***REMOVED***
+  return name;
+***REMOVED***;
+
+const price = () => randomFloat(0, 100, 2);
+
+const randomBool = () => randomInt(1, 2) === 1;
+
+module.exports = {
+  wordBank, randomInt, randomFloat, itemName, price, randomBool,
+***REMOVED***;
