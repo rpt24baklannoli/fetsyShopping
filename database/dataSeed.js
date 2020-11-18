777f7e81bb714b6427f24cf0cc5a23401ab224fd
Sunit Mody <sunitmody@gmail.com>
2020-11-18 14:14:33 -0800

    Add jest, jest-extended and randomInt test

--- database/dataSeed.js
+++ database/dataSeed.js
@@ -17,39 +17,6 @@ const seedTable = (numberOfRows) => {
 
     row--;
   }
-
-  // db.query('DROP DATABASE IF EXISTS fetsy')
-  //   .then(() => {
-  //     db.query('CREATE DATABASE fetsy')
-  //       .then(() => {
-  //         db.query('\c fetsy')
-  //           .then(() => {
-  //             db.query(`CREATE TABLE IF NOT EXISTS items (
-  //               item_id SERIAL PRIMARY KEY,
-  //               item_name VARCHAR(150),
-  //               best_seller BOOLEAN,
-  //               price float(2),
-  //               price_reduction float(2),
-  //               in_stock BOOLEAN,
-  //               us_free_shipping BOOLEAN,
-  //               carts_item_is_in INT
-  //             )`)
-  //               .then(() => {
-  //                 while (row > 0) {
-  //                   const price = data.price();
-  //                   const priceReduction = data.randomFloat(0, price, 2);
-
-  //                   db
-  //                     .query(`INSERT INTO items (item_name, best_seller, price, price_reduction, in_stock, us_free_shipping, carts_item_is_in) VALUES ('${data.itemName()}', ${data.randomBool()}, ${price}, ${priceReduction}, ${data.randomBool()}, ${data.randomBool()}, ${data.randomInt(0, 20)})`)
-  //                     .catch((error) => console.error('Error saving item to database', error));
-
-  //                   row--;
-  //                 }
-  //               })
-  //               .catch((error) => console.error('Error setting up the database', error));
-  //           });
-  //       });
-  //   });
 };
 
 // Seed db with a certain number of rows of data
