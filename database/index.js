42be2599520ddf5279fffc4758cec07c1de6cee2
Sunit Mody <sunitmody@gmail.com>
2020-12-29 11:05:06 -0800

    Modified seed script and database user to "root"

--- database/index.js
+++ database/index.js
@@ -12,7 +12,7 @@
 const { Pool ***REMOVED*** = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
+  user: 'root',
   host: 'localhost',
   database: 'fetsy',
 ***REMOVED***);
--- login.sh
+++ login.sh
@@ -1,1 +1,1 @@
-ssh -i "/Users/sunitmody/Documents/RPT-24/rpt24-fec/fetsyShopping/fetsyShopping.pem" ec2-user@ec2-13-59-222-205.us-east-2.compute.amazonaws.com
+ssh -i "/Users/sunitmody/Documents/RPT-24/rpt24-fec/fetsyShopping/fetsyShopping.pem" ec2-user@ec2-18-189-21-103.us-east-2.compute.amazonaws.com
