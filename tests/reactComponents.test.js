5d65aa47aaa802d940eaef87129236b47ee09d04
Sunit Mody <sunitmody@gmail.com>
2020-12-30 17:39:15 -0800

    Modified reactComponents.test

--- tests/reactComponents.test.js
+++ tests/reactComponents.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { shallow, mount, render ***REMOVED*** from 'enzyme';
 
 import CartCol from '../client/src/components/cartCol.jsx';
-import BuyBox from '../client/src/components/buyBox.jsx';
-import ShoppingRegion from '../client/src/components/shoppingRegion.jsx';
+import NameBestSeller from '../client/src/components/nameBestSeller.jsx';
+import FreeShippingBundle from '../client/src/components/freeShippingBundle.jsx';
 import Seller from '../client/src/components/seller.jsx';
 
 //## UNIT TESTING REACT COMPONENTS (USING ENZYME)
@@ -14,38 +14,44 @@ describe('React components render correctly - testing', () => {
     shallow(<CartCol />);
 ***REMOVED***);
 
-  // This test will need to be modified once react componenets are
-  // correctly rendered
-  test("BuyBox renders component header", () => {
-    const wrapper = shallow(<BuyBox />);
-    const header = <h2>BuyBox Component</h2>;
-    expect(wrapper.contains(header)).toEqual(true);
+  // This test needs to be modified. BuyBox no longer has a header.
+  test("NameBestSeller renders component span", () => {
+    const wrapper = shallow(<NameBestSeller />);
+    const span = <BestSellerSpan>Bestseller</BestSellerSpan>;
+    expect(wrapper.contains(span)).toEqual(true);
 ***REMOVED***);
+
+
 
 ***REMOVED***)
 
 describe('Props are being passed down correctly - testing', () => {
 
   const data = {
-    item_id: 10,
-    item_name: 'consequat aute laboris eiusmod velit dolore pariatur ut commodo ',
-    best_seller: true,
-    price: '85.01',
-    price_reduction: '69.97',
+    best_seller: false,
+    carts_item_is_in: 20,
     in_stock: false,
-    us_free_shipping: true,
-    carts_item_is_in: 4,
-    seller_id: 10,
+    item_id: 2,
+    item_name: "tea monitor drink dollar cat to bed artisan dollar ",
+    on_etsy_since: 2020,
+    price: "94.33",
+    price_reduction: "44.79",
+    recommendedItemImages: [
+      {item_id: 3, image_url: "https://festy-images.s3-us-west-1.amazonaws.com/3_003.jpg", item_name: "bed blue warming ", price: "23.08"***REMOVED***,
+      {item_id: 5, image_url: "https://festy-images.s3-us-west-1.amazonaws.com/5_001.jpg", item_name: "glass glass cloth alcohol cat drink glass sport ball plant ", price: "80.55"***REMOVED***,
+      {item_id: 5, image_url: "https://festy-images.s3-us-west-1.amazonaws.com/5_001.jpg", item_name: "glass glass cloth alcohol cat drink glass sport ball plant ", price: "80.55"***REMOVED***
+***REMOVED***,
+    seller_city: "West Jasmin",
+    seller_id: 3,
+    seller_name: "Drake.Raynor9",
     seller_rating: 3,
-    total_sales: 1012,
-    seller_name: 'Kylie60',
-    seller_city: 'Lake Botown',
-    seller_state: 'Massachusetts',
-    on_etsy_since: 2019
+    seller_state: "Kansas",
+    total_sales: 562,
+    us_free_shipping: true
 ***REMOVED***
 
-  test("shopping region accepts serviceData props", () => {
-    const wrapper = mount(<ShoppingRegion data={data***REMOVED*** />);
+  test("freeShippingBundle accepts serviceData props", () => {
+    const wrapper = mount(<FreeShippingBundle data={data***REMOVED*** />);
     expect(wrapper.props().data).toEqual(data);
 ***REMOVED***);
 
