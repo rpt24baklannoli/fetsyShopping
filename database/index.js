d1a16f981cce44da83d4d2277b49610dc15b8dca
Reed Bruskin <reedbruskin@google.com>
2021-01-22 18:23:09 -0800

    Added update (PUT) method to server and DB

--- database/index.js
+++ database/index.js
@@ -18,29 +18,46 @@ const pool = new Pool({
   database: 'fetsy',
 });
 
-// const model = {
-//   post: (data, cb) => {
-//     pool.query('INSERT INTO items(item_name, best_seller, price, price_reduction, in_stock, us_free_shipping, carts_item_is_in) VALUES($1, $2, $3, $4, $5, $6, $7)', [data.item_name, data.best_seller, data.price, data.price_reduction, data.in_stock, data.us_free_shipping, data.carts_item_is_in], (err, data) => {
-//       if (err) {
-//         cb(err, null)
-//       } else {
-//         cb(null, data)
-//       }
-//     })
-//   }
-// }
+const model = {
+  post: (data, cb) => {
+    pool.query('INSERT INTO items(item_name, best_seller, price, price_reduction, in_stock, us_free_shipping, carts_item_is_in) VALUES($1, $2, $3, $4, $5, $6, $7)', [data.item_name, data.best_seller, data.price, data.price_reduction, data.in_stock, data.us_free_shipping, data.carts_item_is_in], (err, result) => {
+      if (err) {
+        cb(err, null)
+      } else {
+        cb(null, result)
+      }
+    })
+  },
 
-let post = (data, cb) => {
-  // console.log('database data:', data);
-  pool.query('INSERT INTO items(item_name, best_seller, price, price_reduction, in_stock, us_free_shipping, carts_item_is_in) VALUES($1, $2, $3, $4, $5, $6, $7)', [data.item_name, data.best_seller, data.price, data.price_reduction, data.in_stock, data.us_free_shipping, data.carts_item_is_in], (err, data) => {
-    if (err) {
-      cb(err, null)
-    } else {
-      cb(null, data)
-    }
-  })
-};
+  update: (itemId, data, cb) => {
+    // console.log('update database id:', data)
+    pool.query('UPDATE items SET item_name = $1, best_seller = $2, price = $3, price_reduction = $4, in_stock = $5, us_free_shipping = $6, carts_item_is_in = $7 WHERE item_id = $8',
+    [data.item_name, data.best_seller, data.price, data.price_reduction, data.in_stock, data.us_free_shipping, data.carts_item_is_in, itemId],
+    (err, result) => {
+      if (err) {
+        cb(err,  null)
+      } else {
+        cb(null, result)
+      }
+    })
+  },
+
+  delete: (id, cb) => {
+    console.log('delete database id:', id)
+  }
+}
+
+// let post = (data, cb) => {
+//   // console.log('database data:', data);
+//   pool.query('INSERT INTO items(item_name, best_seller, price, price_reduction, in_stock, us_free_shipping, carts_item_is_in) VALUES($1, $2, $3, $4, $5, $6, $7)', [data.item_name, data.best_seller, data.price, data.price_reduction, data.in_stock, data.us_free_shipping, data.carts_item_is_in], (err, data) => {
+//     if (err) {
+//       cb(err, null)
+//     } else {
+//       cb(null, data)
+//     }
+//   })
+// };
 
 module.exports = pool;
-// module.exports.model = model;
-module.exports.post = post;
+module.exports.model = model;
+// module.exports.post = post;
