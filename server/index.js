48f74a6009144bff6c1bb539d98b1241187c312e
Reed Bruskin <reedbruskin@google.com>
2021-01-22 17:57:13 -0800

    Post request successfully created

--- server/index.js
+++ server/index.js
@@ -12,14 +12,13 @@ app.use('/items/:itemId', express.static('client/dist'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false***REMOVED***));
 
-//== This GET request is not being used ==//
-// Get all item data from db
+// Read (Get) all item data from db
 app.get('/shopping/items', (req, res) => {
   console.log('GET request for all items successful');
   db
     .query('SELECT***REMOVED*** FROM items')
     .then((result) => {
-      console.log(result.rows);
+      // console.log('get server req:', result.rows);
       res.send(result.rows);
   ***REMOVED***)
     .catch((error) => {
@@ -27,6 +26,26 @@ app.get('/shopping/items', (req, res) => {
       res.send('Error getting data from db');
   ***REMOVED***);
 ***REMOVED***);
+
+// Create new item
+app.post('/shopping/items', (req, res) => {
+  db.post(req.body, (err, data) => {
+    if (err) {
+      console.log(`failed to insert post request: ${err***REMOVED***`)
+      res.status(400).send(err);
+  ***REMOVED*** else {
+      res.status(200).send(data);
+  ***REMOVED***
+***REMOVED***)
+***REMOVED***)
+
+// Update (put)
+app.put('/shopping/items/:itemId', (req, res) => {
+  db.put
+***REMOVED***)
+
+// Delete (delete)
+
 
 // Get data based on one item Id
 app.get('/shopping/items/:itemId', (req, res) => {
@@ -36,62 +55,62 @@ app.get('/shopping/items/:itemId', (req, res) => {
     .query(`SELECT***REMOVED*** FROM items WHERE item_id = ${itemId***REMOVED***`);
 // console.log('item data promise:', itemDataPromise)
 
-  db.query(`SELECT***REMOVED*** FROM items WHERE item_id = ${itemId***REMOVED***`)
-  .then((res) => {
-    console.log('GET REQUEST:', res.rows)
-***REMOVED***)
+  // db.query(`SELECT***REMOVED*** FROM items WHERE item_id = ${itemId***REMOVED***`)
+  // .then((res) => {
+  //   console.log('GET REQUEST:', res.rows)
+  // ***REMOVED***)
 
 
-  // // Seller Service Amazon EC2 Instance
-  // // http://3.21.248.149:3005/items/2/
-  // const sellerDataPromise = axios
-  //   .get(`http://localhost:3005/items/${itemId***REMOVED***/seller`);
+  // Seller Service Amazon EC2 Instance
+  // http://3.21.248.149:3005/items/2/
+  const sellerDataPromise = axios
+    .get(`http://localhost:3005/items/${itemId***REMOVED***/seller`);
 
-  // // Item Images Service Amazon EC2 Instance
-  // // http://13.52.213.118:3006/items/1/
-  // const itemImagesPromise = axios
-  //   .get('http://13.52.213.118:3006/item/images/distinct');
+  // Item Images Service Amazon EC2 Instance
+  // http://13.52.213.118:3006/items/1/
+  const itemImagesPromise = axios
+    .get('http://13.52.213.118:3006/item/images/distinct');
 
-  // Promise.all([itemDataPromise, sellerDataPromise, itemImagesPromise])
-  //   .then((result) => {
-  //     const itemData = result[0].rows[0];
-  //     const sellerData = result[1].data.rows[0];
-  //     const itemImages = result[2].data.rows;
+  Promise.all([itemDataPromise, sellerDataPromise, itemImagesPromise])
+    .then((result) => {
+      const itemData = result[0].rows[0];
+      const sellerData = result[1].data.rows[0];
+      const itemImages = result[2].data.rows;
 
-  //     const imageOne = itemImages[utils.randomInt(1, 10)];
-  //     const imageTwo = itemImages[utils.randomInt(1, 10)];
-  //     const imageThree = itemImages[utils.randomInt(1, 10)];
+      const imageOne = itemImages[utils.randomInt(1, 10)];
+      const imageTwo = itemImages[utils.randomInt(1, 10)];
+      const imageThree = itemImages[utils.randomInt(1, 10)];
 
-  //     console.log(imageOne, imageTwo, imageThree);
+      console.log(imageOne, imageTwo, imageThree);
 
-  //     const imageOneItemNamePromise = db
-  //       .query(`SELECT DISTINCT item_name, price FROM items WHERE item_id = ${imageOne.item_id***REMOVED***`);
+      const imageOneItemNamePromise = db
+        .query(`SELECT DISTINCT item_name, price FROM items WHERE item_id = ${imageOne.item_id***REMOVED***`);
 
-  //     const imageTwoItemNamePromise = db
-  //       .query(`SELECT DISTINCT item_name, price FROM items WHERE item_id = ${imageTwo.item_id***REMOVED***`);
+      const imageTwoItemNamePromise = db
+        .query(`SELECT DISTINCT item_name, price FROM items WHERE item_id = ${imageTwo.item_id***REMOVED***`);
 
-  //     const imageThreeItemNamePromise = db
-  //       .query(`SELECT DISTINCT item_name, price FROM items WHERE item_id = ${imageThree.item_id***REMOVED***`);
+      const imageThreeItemNamePromise = db
+        .query(`SELECT DISTINCT item_name, price FROM items WHERE item_id = ${imageThree.item_id***REMOVED***`);
 
-  //     Promise.all([imageOneItemNamePromise, imageTwoItemNamePromise, imageThreeItemNamePromise])
-  //       .then((names) => {
-  //         const nameOne = { item_name: names[0].rows[0].item_name ***REMOVED***;
-  //         const priceOne = { price: names[0].rows[0].price ***REMOVED***;
-  //         const nameTwo = { item_name: names[1].rows[0].item_name ***REMOVED***;
-  //         const priceTwo = { price: names[1].rows[0].price ***REMOVED***;
-  //         const nameThree = { item_name: names[2].rows[0].item_name ***REMOVED***;
-  //         const priceThree = { price: names[2].rows[0].price ***REMOVED***;
+      Promise.all([imageOneItemNamePromise, imageTwoItemNamePromise, imageThreeItemNamePromise])
+        .then((names) => {
+          const nameOne = { item_name: names[0].rows[0].item_name ***REMOVED***;
+          const priceOne = { price: names[0].rows[0].price ***REMOVED***;
+          const nameTwo = { item_name: names[1].rows[0].item_name ***REMOVED***;
+          const priceTwo = { price: names[1].rows[0].price ***REMOVED***;
+          const nameThree = { item_name: names[2].rows[0].item_name ***REMOVED***;
+          const priceThree = { price: names[2].rows[0].price ***REMOVED***;
 
-  //         const recommendedItemImages = { recommendedItemImages: [{ ...imageOne, ...nameOne, ...priceOne ***REMOVED***, { ...imageTwo, ...nameTwo, ...priceTwo ***REMOVED***, { ...imageThree, ...nameThree, ...priceThree ***REMOVED***] ***REMOVED***;
+          const recommendedItemImages = { recommendedItemImages: [{ ...imageOne, ...nameOne, ...priceOne ***REMOVED***, { ...imageTwo, ...nameTwo, ...priceTwo ***REMOVED***, { ...imageThree, ...nameThree, ...priceThree ***REMOVED***] ***REMOVED***;
 
-  //         const serviceData = { ...itemData, ...sellerData, ...recommendedItemImages ***REMOVED***;
+          const serviceData = { ...itemData, ...sellerData, ...recommendedItemImages ***REMOVED***;
 
-  //         res.send(serviceData);
-  //     ***REMOVED***)
-        // .catch((error) => {
-        //   console.error(error);
-        // ***REMOVED***);
-    // ***REMOVED***)
+          res.send(serviceData);
+      ***REMOVED***)
+        .catch((error) => {
+          console.error(error);
+      ***REMOVED***);
+  ***REMOVED***)
     .catch((error) => {
       console.error(error);
   ***REMOVED***);
