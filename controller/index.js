2db4127757c6605301c094c434ec67cc85e06cb8
Reed Bruskin <reedbruskin@google.com>
2021-01-25 15:57:03 -0800

    Changed CRUD behavior to follow MVC pattern -Requests now flow react > server > controller > model and back

---
+++ controller/index.js
@@ -0,0 +1,68 @@
+const model = require('../model/index.js')
+
+const shopping = {
+  getOne: (itemId) => {
+    return new Promise ((resolve, reject) => {
+      model.model.getOne(itemId)
+      .then((data) => {
+        resolve(data);
+    ***REMOVED***)
+      .catch((err) => {
+        reject(err);
+    ***REMOVED***);
+  ***REMOVED***)
+***REMOVED***
+
+  getAll: () => {
+    return new Promise ((resolve, reject) => {
+      model.model.getAll()
+      .then((data) => {
+        resolve(data);
+    ***REMOVED***)
+      .catch((err) => {
+        reject(err);
+    ***REMOVED***)
+  ***REMOVED***)
+***REMOVED***
+
+  create: (newItemData) => {
+    return new Promise ((resolve, reject) => {
+      model.model.post(newItemData)
+      .then((response) => {
+        resolve(response);
+    ***REMOVED***)
+      .catch((err) => {
+        reject(err);
+    ***REMOVED***)
+  ***REMOVED***)
+***REMOVED***
+
+  update: (itemId, updatedData) => {
+    return new Promise ((resolve, reject) => {
+      model.model.update(itemId, updatedData)
+      .then((response) => {
+        resolve(response);
+    ***REMOVED***)
+      .catch((err) => {
+        reject(err);
+    ***REMOVED***)
+  ***REMOVED***)
+***REMOVED***
+
+  delete: (itemId) => {
+    return new Promise ((resolve, reject) => {
+      model.model.delete(itemId)
+      .then((response) => {
+        resolve(response);
+    ***REMOVED***)
+      .catch((err) => {
+        reject(err);
+    ***REMOVED***)
+  ***REMOVED***)
+***REMOVED***
+
+***REMOVED***;
+
+
+
+module.exports.shopping = shopping;
