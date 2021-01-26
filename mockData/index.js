cda513a532e4a0fbe181b6029c29d6061557fdda
Reed Bruskin <reedbruskin@google.com>
2021-01-26 12:04:54 -0800

    Still in progress: pulling together all 3 service's get requests -Added a "getDistinct" get request -Added mock data to simulate a get request to the image service -Added get request to seller's service -Corrected gitignore to include index.html -Added index.html so that React could render

---
+++ mockData/index.js
@@ -0,0 +1,44 @@
+let mockImages = [
+  {
+    image_id: 1,
+    image_urls: ['https://picsum.photos/id/1/200', 'https://picsum.photos/id/2/200', 'https://picsum.photos/id/3/200', 'https://picsum.photos/id/4/200', 'https://picsum.photos/id/5/200', 'https://picsum.photos/id/6/200', 'https://picsum.photos/id/7/200', 'https://picsum.photos/id/8/200', 'https://picsum.photos/id/9/200', 'https://picsum.photos/id/10/200']
+***REMOVED***
+  {
+    image_id: 2,
+    image_urls: ['https://picsum.photos/id/1/200', 'https://picsum.photos/id/2/200', 'https://picsum.photos/id/3/200', 'https://picsum.photos/id/4/200', 'https://picsum.photos/id/5/200', 'https://picsum.photos/id/6/200', 'https://picsum.photos/id/7/200', 'https://picsum.photos/id/8/200', 'https://picsum.photos/id/9/200', 'https://picsum.photos/id/10/200']
+***REMOVED***
+  {
+    image_id: 3,
+    image_urls: ['https://picsum.photos/id/1/200', 'https://picsum.photos/id/2/200', 'https://picsum.photos/id/3/200', 'https://picsum.photos/id/4/200', 'https://picsum.photos/id/5/200', 'https://picsum.photos/id/6/200', 'https://picsum.photos/id/7/200', 'https://picsum.photos/id/8/200', 'https://picsum.photos/id/9/200', 'https://picsum.photos/id/10/200']
+***REMOVED***
+  {
+    image_id: 4,
+    image_urls: ['https://picsum.photos/id/1/200', 'https://picsum.photos/id/2/200', 'https://picsum.photos/id/3/200', 'https://picsum.photos/id/4/200', 'https://picsum.photos/id/5/200', 'https://picsum.photos/id/6/200', 'https://picsum.photos/id/7/200', 'https://picsum.photos/id/8/200', 'https://picsum.photos/id/9/200', 'https://picsum.photos/id/10/200']
+***REMOVED***
+  {
+    image_id: 5,
+    image_urls: ['https://picsum.photos/id/1/200', 'https://picsum.photos/id/2/200', 'https://picsum.photos/id/3/200', 'https://picsum.photos/id/4/200', 'https://picsum.photos/id/5/200', 'https://picsum.photos/id/6/200', 'https://picsum.photos/id/7/200', 'https://picsum.photos/id/8/200', 'https://picsum.photos/id/9/200', 'https://picsum.photos/id/10/200']
+***REMOVED***
+  {
+    image_id: 6,
+    image_urls: ['https://picsum.photos/id/1/200', 'https://picsum.photos/id/2/200', 'https://picsum.photos/id/3/200', 'https://picsum.photos/id/4/200', 'https://picsum.photos/id/5/200', 'https://picsum.photos/id/6/200', 'https://picsum.photos/id/7/200', 'https://picsum.photos/id/8/200', 'https://picsum.photos/id/9/200', 'https://picsum.photos/id/10/200']
+***REMOVED***
+  {
+    image_id: 7,
+    image_urls: ['https://picsum.photos/id/1/200', 'https://picsum.photos/id/2/200', 'https://picsum.photos/id/3/200', 'https://picsum.photos/id/4/200', 'https://picsum.photos/id/5/200', 'https://picsum.photos/id/6/200', 'https://picsum.photos/id/7/200', 'https://picsum.photos/id/8/200', 'https://picsum.photos/id/9/200', 'https://picsum.photos/id/10/200']
+***REMOVED***
+  {
+    image_id: 8,
+    image_urls: ['https://picsum.photos/id/1/200', 'https://picsum.photos/id/2/200', 'https://picsum.photos/id/3/200', 'https://picsum.photos/id/4/200', 'https://picsum.photos/id/5/200', 'https://picsum.photos/id/6/200', 'https://picsum.photos/id/7/200', 'https://picsum.photos/id/8/200', 'https://picsum.photos/id/9/200', 'https://picsum.photos/id/10/200']
+***REMOVED***
+  {
+    image_id: 9,
+    image_urls: ['https://picsum.photos/id/1/200', 'https://picsum.photos/id/2/200', 'https://picsum.photos/id/3/200', 'https://picsum.photos/id/4/200', 'https://picsum.photos/id/5/200', 'https://picsum.photos/id/6/200', 'https://picsum.photos/id/7/200', 'https://picsum.photos/id/8/200', 'https://picsum.photos/id/9/200', 'https://picsum.photos/id/10/200']
+***REMOVED***
+  {
+    image_id: 10,
+    image_urls: ['https://picsum.photos/id/1/200', 'https://picsum.photos/id/2/200', 'https://picsum.photos/id/3/200', 'https://picsum.photos/id/4/200', 'https://picsum.photos/id/5/200', 'https://picsum.photos/id/6/200', 'https://picsum.photos/id/7/200', 'https://picsum.photos/id/8/200', 'https://picsum.photos/id/9/200', 'https://picsum.photos/id/10/200']
+***REMOVED***
+];
+
+module.exports.mockImages = mockImages;
