84cd5fc8d47bf1b22e185b2eed4387907294baad
Reed Bruskin <reedbruskin@google.com>
2021-02-27 12:30:55 -0800

    Jest changes to work with CircleCI
    - Added unique key to sellerRating component to eliminate that warning
    - Corrected the async issue with the Delete CRUD operation

--- client/src/components/sellerRating.jsx
+++ client/src/components/sellerRating.jsx
@@ -20,7 +20,7 @@ const StarImg = styled.img`
 
 function SellerRating(props) {
   const { data ***REMOVED*** = props;
-  const stars = [...Array(data.seller_rating)].map((star) => <StarImg src="https://festy-images.s3.us-west-1.amazonaws.com/star.png" alt="star icon" />);
+  const stars = [...Array(data.seller_rating)].map((star, index) => <StarImg key={index***REMOVED*** src="https://festy-images.s3.us-west-1.amazonaws.com/star.png" alt="star icon" />);
 
   return (
     <SellerRatingSpan>
