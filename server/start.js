6f175590ee758d62f0f58834ec250a00ec2b4067
Reed Bruskin <reedbruskin@google.com>
2021-03-10 22:03:30 -0800

    Implemented Redis caching

--- server/start.js
+++ server/start.js
@@ -1,6 +1,6 @@
 const newrelic = require('newrelic');
 const app = require('./index.js');
-const port = 3004;
+const port = process.env.PORT || 3004;
 
 app.listen(port, () => {
   console.log(`Fetsy shopping listening at port ${port}`);
